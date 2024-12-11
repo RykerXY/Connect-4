70c496aa9f862ff7328f9ee6a7b805a218278f29
RykerXY <115035392+RykerXY@users.noreply.github.com>
2024-12-11 21:35:30 +0700

    Improve UI

--- Assets/Menu Script/startButton.cs
+++ Assets/Menu Script/startButton.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,19 +5,7 @@ using UnityEngine.SceneManagement;
 public class startButton : MonoBehaviour
 {
     public SceneAsset sceneToLoad;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnMouseDown()
+    public void OnMouseDown()
     {
         string sceneName = sceneToLoad != null ? sceneToLoad.name : "";
         if (!string.IsNullOrEmpty(sceneName))
